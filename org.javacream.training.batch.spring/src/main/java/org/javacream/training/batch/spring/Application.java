6423222e582fb72c9f5cb19ba25d79d51f228090
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-08-28 11:20:20 +0200

    initial setup

--- org.javacream.training.batch.spring/src/main/java/org/javacream/training/batch/spring/Application.java
+++ org.javacream.training.batch.spring/src/main/java/org/javacream/training/batch/spring/Application.java
@@ -1,4 +1,4 @@
-package org.javacream.training.spring.batch;
+package org.javacream.training.batch.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- org.javacream.training.batch.spring/src/test/java/org/javacream/training/batch/spring/ApplicationTests.java
+++ org.javacream.training.batch.spring/src/test/java/org/javacream/training/batch/spring/ApplicationTests.java
@@ -1,4 +1,4 @@
-package org.javacream.training.spring.batch;
+package org.javacream.training.batch.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
