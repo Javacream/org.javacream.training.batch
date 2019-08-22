e12bc2d2437487943d3175fa1149461df5342dff
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-08-22 12:16:09 +0200

    add simple batch tasklet

--- org.javacream.training.batch.spring/src/main/java/org/javacream/training/batch/spring/demo/SimpleBatchTaskConfiguration.java
+++ org.javacream.training.batch.spring/src/main/java/org/javacream/training/batch/spring/demo/SimpleBatchTaskConfiguration.java
@@ -1,40 +1,40 @@
-package org.javacream.training.batch.spring.job;
+package org.javacream.training.batch.spring.demo;
 
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.job.builder.SimpleJobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.TaskletStep;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
-@Configuration
 @EnableBatchProcessing
-@ComponentScan
-public class SimpleJobConfiguration {
+@Configuration
+public class SimpleBatchTaskConfiguration {
 
 	@Autowired
 	private JobBuilderFactory jobBuilderFactory;
-
 	@Autowired
 	private StepBuilderFactory stepBuilderFactory;
-
-	@Autowired SimpleTasklet simpleTasklet;
+	@Autowired
+	private SimpleBatchTask simpleTasklet;
 
 	@Bean
-	public Step step() {
-		return stepBuilderFactory.get("step").tasklet(simpleTasklet).build();
+	TaskletStep simpleStep() {
+		TaskletStep step = stepBuilderFactory.get("step").tasklet(simpleTasklet).build();
+		return step;
 	}
-	
+
 	@Bean
 	public Job job() throws Exception {
-		JobBuilder jobStart = jobBuilderFactory.get("job1").incrementer(new RunIdIncrementer());
-		SimpleJobBuilder startStep = jobStart.start(step());
-		return startStep.build();
+		JobBuilder jobBuilder = jobBuilderFactory.get("job1").incrementer(new RunIdIncrementer());
+		SimpleJobBuilder startJob = jobBuilder.start(simpleStep());
+		return startJob.build();
 	}
+
 }
