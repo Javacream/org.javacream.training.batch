255ea3b0b0a72996114088e1bb230f768c734439
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-09-12 12:10:39 +0200

    initialize

--- org.javacream.training.batch.spring/src/main/java/org/javacream/training/batch/spring/simple/BatchConfiguration.java
+++ org.javacream.training.batch.spring/src/main/java/org/javacream/training/batch/spring/simple/BatchConfiguration.java
@@ -1,29 +1,9 @@
 package org.javacream.training.batch.spring.simple;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
-
-	@PostConstruct public void init() {
-		System.out.println("******************");
-	}
-	
-	@Bean @Qualifier("var1") @Scope("singleton") public SimpleBusinessClass simpleBusinessClassVariant1(@Value ("${var1}") String var1, SimpleBusinessComponent simpleBusinessComponent) {
-		System.out.println("################   retrieving " + simpleBusinessComponent);
-		SimpleBusinessClass instance = new SimpleBusinessClass(var1);
-		return instance;
-	}
-	@Bean @Qualifier("var2") public SimpleBusinessClass simpleBusinessClassVariant2(@Value ("${var2}") String var2) {
-		SimpleBusinessClass instance = new SimpleBusinessClass(var2);
-		return instance;
-	}
 }
--- org.javacream.training.batch.spring/src/test/java/org/javacream/training/batch/spring/ApplicationTests.java
+++ org.javacream.training.batch.spring/src/test/java/org/javacream/training/batch/spring/ApplicationTests.java
@@ -1,15 +1,7 @@
 package org.javacream.training.batch.spring;
 
-import javax.sql.DataSource;
-
-import org.javacream.training.batch.spring.simple.SimpleBusinessClass;
-import org.javacream.training.batch.spring.simple.SimpleBusinessComponent;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -17,19 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ApplicationTests {
 
-	@Autowired private DataSource ds;
-	@Autowired private JobBuilderFactory jobBuilderFactory;
-	@Autowired private SimpleBusinessComponent sbcomponent;
-	@Autowired @Qualifier("var1") private SimpleBusinessClass sbclass;
-	
-	@Value ("${hugo}")private String demo;
 	@Test
 	public void contextLoads() {
-		System.out.println("Datasource=" + ds);
-		System.out.println("JobBuilderFactory=" + jobBuilderFactory);
-		System.out.println("demo=" + demo);
-		System.out.println("sbcomponent=" + sbcomponent);
-		System.out.println("sbclass=" + sbclass.getDescription());
 	}
 
 }
