ae26aa9f3709aad07e4491b026162e7a39c4b151
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-09-12 12:01:32 +0200

    play with injection and scopes

--- org.javacream.training.batch.spring/src/main/java/org/javacream/training/batch/spring/simple/BatchConfiguration.java
+++ org.javacream.training.batch.spring/src/main/java/org/javacream/training/batch/spring/simple/BatchConfiguration.java
@@ -4,8 +4,10 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @EnableBatchProcessing
@@ -15,12 +17,13 @@ public class BatchConfiguration {
 		System.out.println("******************");
 	}
 	
-	@Bean @Qualifier("var1") public SimpleBusinessClass simpleBusinessClassVariant1() {
-		SimpleBusinessClass instance = new SimpleBusinessClass("Variant1");
+	@Bean @Qualifier("var1") @Scope("singleton") public SimpleBusinessClass simpleBusinessClassVariant1(@Value ("${var1}") String var1, SimpleBusinessComponent simpleBusinessComponent) {
+		System.out.println("################   retrieving " + simpleBusinessComponent);
+		SimpleBusinessClass instance = new SimpleBusinessClass(var1);
 		return instance;
 	}
-	@Bean @Qualifier("var2") public SimpleBusinessClass simpleBusinessClassVariant2() {
-		SimpleBusinessClass instance = new SimpleBusinessClass("Variant2");
+	@Bean @Qualifier("var2") public SimpleBusinessClass simpleBusinessClassVariant2(@Value ("${var2}") String var2) {
+		SimpleBusinessClass instance = new SimpleBusinessClass(var2);
 		return instance;
 	}
 }
--- org.javacream.training.batch.spring/src/main/java/org/javacream/training/batch/spring/simple/SimpleBusinessComponent.java
+++ org.javacream.training.batch.spring/src/main/java/org/javacream/training/batch/spring/simple/SimpleBusinessComponent.java
@@ -1,8 +1,16 @@
 package org.javacream.training.batch.spring.simple;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("singleton")
 public class SimpleBusinessComponent {
+	
+	@PostConstruct public void initTheComponent() {
+		System.out.println("########################## initializing " + this);
+	}
 
 }
