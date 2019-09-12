93756f1722465167e5ab15b75ac1fcd9b8b410ca
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-09-12 14:53:11 +0200

    JobParameters and Incrementer

--- org.javacream.training.batch.spring/src/main/java/org/javacream/training/batch/spring/simple/BatchConfiguration.java
+++ org.javacream.training.batch.spring/src/main/java/org/javacream/training/batch/spring/simple/BatchConfiguration.java
@@ -8,9 +8,7 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.job.builder.SimpleJobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.step.builder.SimpleStepBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,20 +18,17 @@ public class BatchConfiguration {
 	@Autowired private JobBuilderFactory jobBuilderFactory;
 	@Autowired private StepBuilderFactory stepBuilderFactory;
 	
-	@Bean @Qualifier("step2") 
-	Step stepTwoLines(SimpleItemReader reader, SimpleItemProcessor processor, SimpleItemWriter writer) {
-		SimpleStepBuilder<String, Integer> builder = stepBuilderFactory.get("step1").chunk(2);
-		builder.reader(reader).processor(processor).writer(writer);
-		return builder.build();
-	}
-	@Bean @Qualifier("step1") Step stepOneLine(SimpleItemReader reader, SimpleItemProcessor processor, SimpleItemWriter writer) {
+	@Bean Step stepOneLine(SimpleItemReader reader, SimpleItemProcessor processor, SimpleItemWriter writer) {
 		return stepBuilderFactory.get("step1").<String, Integer>chunk(3).reader(reader).processor(processor).writer(writer).build();
 	}
 	
 	@Bean
-	public Job job(@Qualifier("step2") Step step) throws Exception {
-		JobBuilder jobBuilder = jobBuilderFactory.get("job1").incrementer(new RunIdIncrementer());
+	public Job job(Step step) throws Exception {
+		JobBuilder jobBuilder = jobBuilderFactory.get("job2");//.incrementer(new RunIdIncrementer());
 		SimpleJobBuilder startJob = jobBuilder.start(step);
 		return startJob.build();
 	}
+
+
 }
+
