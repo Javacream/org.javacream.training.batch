f8ff64a3602575157071d562d02f62e5ff8abbb7
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-09-12 13:20:11 +0200

    add SimpleTasklet

--- org.javacream.training.batch.spring/src/main/java/org/javacream/training/batch/spring/simple/BatchConfiguration.java
+++ org.javacream.training.batch.spring/src/main/java/org/javacream/training/batch/spring/simple/BatchConfiguration.java
@@ -1,9 +1,33 @@
 package org.javacream.training.batch.spring.simple;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.builder.SimpleJobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.TaskletStep;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
+	@Autowired private JobBuilderFactory jobBuilderFactory;
+	@Autowired private StepBuilderFactory stepBuilderFactory;
+	
+	@Bean Step taskletStep(SimpleTasklet simpleTasklet) {
+		TaskletStep step = stepBuilderFactory.get("step1").tasklet(simpleTasklet).build();
+		return step;
+	}
+	
+	@Bean
+	public Job job(Step taskletStep) throws Exception {
+		JobBuilder jobBuilder = jobBuilderFactory.get("job1").incrementer(new RunIdIncrementer());
+		SimpleJobBuilder startJob = jobBuilder.start(taskletStep);
+		return startJob.build();
+	}
 }
---
+++ org.javacream.training.batch.spring/src/main/java/org/javacream/training/batch/spring/simple/SimpleTasklet.java
@@ -0,0 +1,18 @@
+package org.javacream.training.batch.spring.simple;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleTasklet implements Tasklet {
+
+	@Override
+	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+		System.out.println("EXECUTING TASKLET " + this);
+		return RepeatStatus.FINISHED;
+	}
+
+}
