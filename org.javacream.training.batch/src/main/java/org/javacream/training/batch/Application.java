188c5ffb5509a83132c013e94df65bf5a16ecbb0
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-11-29 14:50:06 +0100

    play around with jobs

--- org.javacream.training.batch/src/main/java/org/javacream/training/batch/Application.java
+++ org.javacream.training.batch/src/main/java/org/javacream/training/batch/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		//String[] params = {"name=world", "timestamp=" + System.currentTimeMillis()}; 
 		SpringApplication.run(Application.class, args);
 	}
 
--- org.javacream.training.batch/src/main/java/org/javacream/training/batch/jobs/HelloWorldJobConfiguration.java
+++ org.javacream.training.batch/src/main/java/org/javacream/training/batch/jobs/HelloWorldJobConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -26,9 +25,9 @@ public class HelloWorldJobConfiguration {
 	public Step helloWorldStep() {
 //		HelloWorldTasklet hwt = new HelloWorldTasklet();
 //		Step helloWorldStep = stepBuilderFactory.get("hello-world").tasklet(hwt).build();
-		Step helloWorldStep = stepBuilderFactory.get("hello-world")
+		Step helloWorldStep = stepBuilderFactory.get("goodbye-moon2")
 				.tasklet((StepContribution contribution, ChunkContext chunkContext) -> {
-					System.out.println("Hello World");
+					System.out.println("Goodbye Moon");
 					return RepeatStatus.FINISHED;
 				}).build();
 		return helloWorldStep;
@@ -37,7 +36,7 @@ public class HelloWorldJobConfiguration {
 
 	@Bean
 	public Job helloWorldJob() {
-		return jobBuilderFactory.get("hello-world-job").start(helloWorldStep()).build();
+		return jobBuilderFactory.get("goodbye-moon-job").start(helloWorldStep()).build();
 	}
 
 //	public class HelloWorldTasklet implements Tasklet {
