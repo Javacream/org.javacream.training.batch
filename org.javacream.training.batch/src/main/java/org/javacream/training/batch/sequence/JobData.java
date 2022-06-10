1d263cd01cbd92bf2ed84357b18829b125eaad13
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-06-10 09:52:40 +0200

    fix sample

--- org.javacream.training.batch/src/main/java/org/javacream/training/batch/sequence/JobData.java
+++ org.javacream.training.batch/src/main/java/org/javacream/training/batch/sequence/JobData.java
@@ -3,8 +3,10 @@ package org.javacream.training.batch.sequence;
 import java.io.Serializable;
 
 import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.stereotype.Component;
 
 @JobScope
+@Component
 public class JobData implements Serializable{
 
 	private static final long serialVersionUID = 1L;
