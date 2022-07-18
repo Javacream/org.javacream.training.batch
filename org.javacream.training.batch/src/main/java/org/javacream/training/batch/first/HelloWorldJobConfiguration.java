6c9836055c0f1339d0fa9e0fe34a7873224fc83b
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-18 12:15:53 +0200

    add first contact job and step

---
+++ org.javacream.training.batch/src/main/java/org/javacream/training/batch/first/HelloWorldJobConfiguration.java
@@ -0,0 +1,28 @@
+package org.javacream.training.batch.first;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class HelloWorldJobConfiguration {
+
+	@Autowired JobBuilderFactory jobBuilderFactory;
+	@Autowired StepBuilderFactory stepBuilderFactory;
+	
+	
+	@Bean public Step helloWorldStep() {
+		return stepBuilderFactory.get("hello-world-step").tasklet(new FirstTasklet()).build();
+	}
+	
+	//Spring Boot: Dieser Job wird automatisch (!) gestartet und bekommt als Parametrisierung nichts
+	@Bean public Job helloWorldJob() {
+		return jobBuilderFactory.get("hello-world-job").start(helloWorldStep()).build();
+	}
+}
