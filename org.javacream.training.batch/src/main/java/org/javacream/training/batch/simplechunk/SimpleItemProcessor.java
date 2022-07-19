abfdbf4c554aaae8705b2485dfce70e754909d7d
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-19 11:45:17 +0200

    throw IllegalArgument

--- org.javacream.training.batch/src/main/java/org/javacream/training/batch/simplechunk/SimpleItemProcessor.java
+++ org.javacream.training.batch/src/main/java/org/javacream/training/batch/simplechunk/SimpleItemProcessor.java
@@ -6,11 +6,16 @@ import org.springframework.stereotype.Component;
 
 @Component("processor")
 @StepScope
-public class SimpleItemProcessor implements ItemProcessor<String, Integer>{
+public class SimpleItemProcessor implements ItemProcessor<String, Integer> {
 
 	@Override
 	public Integer process(String item) throws Exception {
-		return item.length();
+		int length = item.length();
+		if (length > 4) {
+			throw new IllegalAccessException("string too long");
+		} else {
+			return length;
+		}
 	}
 
 }
