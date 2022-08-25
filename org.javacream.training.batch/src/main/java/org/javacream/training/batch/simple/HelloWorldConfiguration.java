26d0df1eb86cda2ce56e15e1c0df1422e10f0b9f
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-08-25 14:28:38 +0200

    add web frontend

--- org.javacream.training.batch/src/main/java/org/javacream/training/batch/simple/HelloWorldConfiguration.java
+++ org.javacream.training.batch/src/main/java/org/javacream/training/batch/simple/HelloWorldConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -31,7 +32,8 @@ public class HelloWorldConfiguration {
     }
 
     @Bean
+    @Qualifier("helloWorld")
     public Job helloWorldJob(){
-        return jobBuilderFactory.get("hello-world-job-with-incrementer").incrementer(new RunIdIncrementer()).start(step1()).next(step2()).next(step3()).build();
+        return jobBuilderFactory.get("hello-world-job-with-incrementer").start(step1()).next(step2()).next(step3()).build();
     }
 }
---
+++ org.javacream.training.batch/src/main/java/org/javacream/training/batch/web/JobRestController.java
@@ -0,0 +1,54 @@
+package org.javacream.training.batch.web;
+
+import java.util.Properties;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JobRestController {
+
+	@Autowired
+	private JobLauncher launcher;
+
+	@Autowired
+	@Qualifier("helloWorld")
+	private Job helloWorldJob;
+
+	@PostMapping(path = "api/jobs", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String executeJob(@RequestBody JobLaunchRequest jobLaunchRequest) {
+		System.out.println("received launch request " + jobLaunchRequest);
+		JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
+		Properties props = jobLaunchRequest.getJobParameters();
+
+		for (String paramName : props.stringPropertyNames()) {
+			jobParametersBuilder.addString(paramName, props.getProperty(paramName), true);
+		}
+		JobParameters jobParameters = jobParametersBuilder.toJobParameters();
+		String jobName = jobLaunchRequest.getJobName();
+		try {
+			if ("helloWorld".equals(jobName)) {
+				launcher.run(helloWorldJob, jobParameters);
+			}
+			// else -> Dispatching auf andere Jobs
+		} catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException
+				| JobParametersInvalidException e) {
+			System.err.println(e.getMessage());
+			return e.getMessage();
+		}
+		return "OK";
+	}
+
+}
