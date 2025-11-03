675ef5bbc3fb15fe65f429b653427681ab217852
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-11-03 15:42:40 +0100

    start xml jhob definition

--- org.javacream.training.batch/src/main/java/org/javacream/training/batch/Application.java
+++ org.javacream.training.batch/src/main/java/org/javacream/training/batch/Application.java
@@ -2,8 +2,10 @@ package org.javacream.training.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("jobs/multiStepJob.xml")
 public class Application {
 
 	public static void main(String[] args) {
