ab806fd044ed6a41303e9b93e3df3e084a0f3b32
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-11-03 14:21:06 +0100

    access job parameters

--- org.javacream.training.batch/src/main/java/org/javacream/training/batch/second/HelloMoonTasklet.java
+++ org.javacream.training.batch/src/main/java/org/javacream/training/batch/second/HelloMoonTasklet.java
@@ -1,17 +1,24 @@
 package org.javacream.training.batch.second;
 
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@StepScope
 public class HelloMoonTasklet implements Tasklet{
 
+	@Value("#{jobParameters['name']}")
+	private String greeting;
 	@Override
 	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-		System.out.println("Hello Moon");
+		//var params = chunkContext.getStepContext().getJobParameters();
+		//var greeting = params.get("name");
+		System.out.println("Hello " + greeting);
 		return RepeatStatus.FINISHED;
 	}
 	
