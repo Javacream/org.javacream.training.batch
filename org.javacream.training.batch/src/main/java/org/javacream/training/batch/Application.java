95a9fb6f9b5adf5b907bd7bab9c49af423e3e3b7
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-11-04 15:20:45 +0100

    JSON to File

--- org.javacream.training.batch/src/main/java/org/javacream/training/batch/Application.java
+++ org.javacream.training.batch/src/main/java/org/javacream/training/batch/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource({"jobs/multiStepJob.xml", "jobs/chunkFileXmlJob.xml"})
+@ImportResource({"jobs/multiStepJob.xml", "jobs/chunkFileXmlJob.xml", "jobs/jsonToFileXmlJob.xml"})
 public class Application {
 
 	public static void main(String[] args) {
